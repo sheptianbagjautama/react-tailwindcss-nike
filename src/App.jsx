86f96195af579c6cd57bf623a020e7bbde7f4ee0
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-31 14:04:48 +0700

    init component, section and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
+import Nav from "./components/Nav";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+
 export default function App() {
   return (
     <main className="relative">
-      Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">Popular Products</section>
-      <section className="padding">Super Quality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">Special Offers</section>
-      <section className="padding bg-pale-blue">Customer Reviews</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <section className="padding-x padding-t pb-8 bg-black">Footer</section>
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x padding-t pb-8 bg-black">
+        <Footer />
+      </section>
     </main>
   );
 }
